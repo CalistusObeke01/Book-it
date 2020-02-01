f6b8828c28b354829b3e62898e07d7a232e4c568
Sarah0079ja <sodiavbara@gmail.com>
2020-02-01 23:05:37 +0100

    new1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, {Suspense, lazy} from 'react';
-import { Route, Switch } from "react-router-dom";
+import {Route, Switch } from "react-router-dom";
+// import Confrence from "./components/Confrence";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Loading from './components/Loading';
 import './App.css';
 
 
+
 const Home = lazy(() => import('./routes/Home'));
 const PageNotFound = lazy(() => import('./routes/PageNotFound'));
-
+const Confrence = lazy(() => import('./components/Confrence'));
+ 
 function App() {
   return (
     <>
       <Header />
       <Suspense fallback={<Loading />}>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="*" component={PageNotFound} /> 
-        </Switch>
+        
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/confrence" component={Confrence} />
+            <Route path="*" component={PageNotFound} />
+          </Switch>
+        
       </Suspense>
       <Footer />
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,46 +8,52 @@ import { FaAlignRight, FaTimes } from 'react-icons/fa';
 function Header() {
     const [isOpen, setIsOpen] = useState(false);
 
-    return(
-        <>
-            <header id="navbar">
-                <div className="logo"> 
-                    <Link to="/">
-                        <img src={Logo1} alt="Book it" width="100" className="logo1"/>
-                        <img src={Logo2} alt="Book it" width="120" className="logo2"/>
-                    </Link>
-                </div>
-                <nav className={isOpen ? "show-nav" : "hide-nav"}>
-                    <ul>
-                        <li>
-                            <Link to="/pricing">
-                                Pricing
-                            </Link>
-                        </li>
-                        <li>
-                            <a href="/#features">
-                                Features
-                            </a>
-                        </li>
-                        <li>
-                            <Link to="/contact">
-                                Contact
-                            </Link>
-                        </li>
-                        <li>
-                            <a href="/#sign-up" id="signIn">
-                                Sign In
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <button type="button" className="menu-toggle" onClick={() => setIsOpen(!isOpen)}>
-                    {isOpen ? <FaTimes className="nav-icon" /> : <FaAlignRight className="nav-icon" />}
-                </button>
-            </header>
-
-        </>
-    )
+    return (
+      <>
+        <header id="navbar">
+          <div className="logo">
+            <Link to="/">
+              <img src={Logo1} alt="Book it" width="100" className="logo1" />
+              <img src={Logo2} alt="Book it" width="120" className="logo2" />
+            </Link>
+          </div>
+          <nav className={isOpen ? "show-nav" : "hide-nav"}>
+            <ul>
+              <li>
+                <Link to="/pricing">Pricing</Link>
+              </li>
+              <li>
+                <a href="/#features">Features</a>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li>
+                <a href="/#sign-up" id="signIn">
+                  Sign In
+                </a>
+              </li>
+              <li>
+                <Link to="/confrence">
+                  Confrence
+                  </Link>
+              </li>
+            </ul>
+          </nav>
+          <button
+            type="button"
+            className="menu-toggle"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {isOpen ? (
+              <FaTimes className="nav-icon" />
+            ) : (
+              <FaAlignRight className="nav-icon" />
+            )}
+          </button>
+        </header>
+      </>
+    );
 }
 
 export default Header
