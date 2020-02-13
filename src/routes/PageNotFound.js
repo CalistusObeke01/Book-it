68c16852f177d602ea267a9b5e744c1c2a96b0d4
calistus <howell2028@yahoo.com>
2020-02-13 02:50:10 +0100

    Create 404 page

--- src/routes/PageNotFound.js
+++ src/routes/PageNotFound.js
@@ -1,10 +1,24 @@
-import React from 'react'
-
+import React from 'react';
+import Button from '../components/Button';
+import {Link} from 'react-router-dom';
 function Pagenotfound() {
     return(
-        <div style={{marginTop: '240px'}}>
-            page not found
+        <div className="error-container">
+            <div className="error-text">
+                <p className="error-figure">40<span style={{color: 'var(--primaryColor)'}}>4</span></p>
+                <p >
+                    We've noticed you are lost and confused, dont worry.<br />
+                    We will help you find your next  opportunity
+                </p>
+                <Link to="/"> 
+                    <Button>Go Home</Button>
+                </Link>
+            </div>
+            <div className="error-img">
+
+            </div>
         </div>
+
     )
 }
 
