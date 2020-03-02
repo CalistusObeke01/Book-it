f776d177717fcf82c7bba6c6b52aad0ade17be2d
gboyega <jaiyeolagboyega@gmail.com>
2020-03-02 12:20:51 +0100

    debugging heroku again

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ routes(app);
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/", "index.html"));
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 app.listen(process.env.PORT || 4000, () =>
