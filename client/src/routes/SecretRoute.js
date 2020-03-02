174798a866ca1d5a04f23ee68a5b269bfa008446
gboyega <jaiyeolagboyega@gmail.com>
2020-03-02 13:30:16 +0100

    adding push changes

--- client/src/routes/SecretRoute.js
+++ client/src/routes/SecretRoute.js
@@ -1,23 +1,23 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import { Redirect, Route } from "react-router-dom";
 import { AuthContext } from "../components/AuthContext";
 
 export class SecretRoute extends Component {
-         static contextType = AuthContext;
-         render() {
-           const { component: Component, ...props } = this.props;
+  static contextType = AuthContext;
+  render() {
+    const { component: Component, ...props } = this.props;
 
-           return (
-             <Route
-               {...props}
-               render={props =>
-                 this.context.isAuthenticated /*&& this.context.user.admin*/ ? (
-                   <Component {...props} />
-                 ) : (
-                   <Redirect to="/" />
-                 )
-               }
-             />
-           );
-         }
-       }
+    return (
+      <Route
+        {...props}
+        render={props =>
+          this.context.isAuthenticated && this.context.user.admin ? (
+            <Component {...props} />
+          ) : (
+            <Redirect to="/" />
+          )
+        }
+      />
+    );
+  }
+}
