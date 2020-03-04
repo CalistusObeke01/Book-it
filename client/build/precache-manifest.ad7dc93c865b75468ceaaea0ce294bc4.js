5684489510873ad2d65a7058c8fcc145ad75dcd8
gboyega <jaiyeolagboyega@gmail.com>
2020-03-04 10:08:50 +0100

    adding latest changes to heroku

---
+++ client/build/precache-manifest.ad7dc93c865b75468ceaaea0ce294bc4.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6639a1da547d1618b1291f89da0bc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb03d6c456f4f56b66b3",
+    "url": "/static/css/3.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "470cbbce7d4c1b3bc489",
+    "url": "/static/css/main.6405ec75.chunk.css"
+  },
+  {
+    "revision": "fa677fbbf7173f0d0e30",
+    "url": "/static/js/0.afddf8e1.chunk.js"
+  },
+  {
+    "revision": "eb03d6c456f4f56b66b3",
+    "url": "/static/js/3.be78871c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/3.be78871c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48cdc373e07aa5b00c42",
+    "url": "/static/js/4.4903a4dd.chunk.js"
+  },
+  {
+    "revision": "e9e8342c90f54d85d26d",
+    "url": "/static/js/5.05037fbc.chunk.js"
+  },
+  {
+    "revision": "90704d5a4732bc86fb38",
+    "url": "/static/js/6.04685047.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/6.04685047.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37ecd32420e8b0ca489c",
+    "url": "/static/js/7.f323dbe0.chunk.js"
+  },
+  {
+    "revision": "82cd0fd136758bfa46bb",
+    "url": "/static/js/8.1443ba73.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/8.1443ba73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "470cbbce7d4c1b3bc489",
+    "url": "/static/js/main.e86aff8f.chunk.js"
+  },
+  {
+    "revision": "a19f98e09306ca6672b6",
+    "url": "/static/js/runtime-main.9e461ba0.js"
+  },
+  {
+    "revision": "9db0121006d9aaabb42489c450bc1a32",
+    "url": "/static/media/71d910fe035b5832022357875f97e844deloitte.9db01210.png"
+  },
+  {
+    "revision": "9bc3845675a2e1c563c49b1e630e1fab",
+    "url": "/static/media/Group 116.9bc38456.png"
+  },
+  {
+    "revision": "b63306a1162655639c201c66f85857f7",
+    "url": "/static/media/Picture1111.b63306a1.png"
+  },
+  {
+    "revision": "54c7cd33a08d8968a482b06360cad5ad",
+    "url": "/static/media/South-Africa.54c7cd33.png"
+  },
+  {
+    "revision": "204a4d4d14d6291c717643192adc6b90",
+    "url": "/static/media/benjamin-child-GWe-1.204a4d4d.png"
+  },
+  {
+    "revision": "6a1f5efe49949477e189742ae717e43d",
+    "url": "/static/media/confushed.6a1f5efe.png"
+  }
+]);
