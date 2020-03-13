e6675a9d476464c5a10869de2b4cac26c13005b0
Calistus Obeke <howell2028@yahoo.com>
2020-03-13 09:31:35 +0100

    Modification

--- client/src/components/HowToUse.js
+++ client/src/components/HowToUse.js
@@ -7,10 +7,10 @@ import logoCheapCost from "../images/Path 156.png";
 function HowToUse({ children, usage}) {
     return (
         <div className="how-to-use">
-            <div className="col-md-6 col-sm-12">
+            <div className="col-md-6">
                 <img src={howtouselogo} alt="How to use app" className="img-responsive" width="100%" />
             </div>
-            <div className="col-md-6 col-sm-12">
+            <div className="col-md-6">
                 <p className="text-right how-to-use-text">
                     <b>Book!T</b> is one of the fast growing platforms 
                      brands use for managing their spaces
